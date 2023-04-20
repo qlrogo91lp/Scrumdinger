9c338f99ddedef40c5970850ce262ee5b3c49227
yjkim <qlrogo91lp@gmail.com>
2023-04-20 11:55:30 +0900

    Chapter 1

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by 윤재 on 2023/04/20.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(/*@START_MENU_TOKEN@*/.caption/*@END_MENU_TOKEN@*/)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(/*@START_MENU_TOKEN@*/.caption/*@END_MENU_TOKEN@*/)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
+            
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+            }
+            .accessibilityLabel("Next Speaker")
+            
+        }
+        .padding()
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
